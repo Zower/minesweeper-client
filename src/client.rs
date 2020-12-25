e2a866dd681319bca8d13f7a597021260aad804d
Eric <eirtob98@hotmail.com>
2020-12-25 18:14:02 +0100

    Program now borrows correctly

--- src/client.rs
+++ src/client.rs
@@ -1,111 +1,108 @@
+use std::fmt;
+use std::fmt::Formatter;
+
 use minesweeper::minesweeper_client::MinesweeperClient;
 use minesweeper::*;
 
-pub mod minesweeper{
+pub mod minesweeper {
     tonic::include_proto!("proto");
 }
-
 struct Game {
     level_id: String,
     rows: i32,
     columns: i32,
-    mines: i32
+    mines: i32,
 }
 impl Game {
+    async fn init(
+        &mut self,
+        client: &mut MinesweeperClient<tonic::transport::Channel>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let response = self.start_game(client, &self.level_id).await?;
 
-    async fn init(&mut self, client: &mut MinesweeperClient<tonic::transport::Channel>) -> Result<(), Box<dyn std::error::Error>> {
-        println!("Rows: {}", self.rows);
-        println!("Columns: {}", self.columns);
-        println!("Mines: {}", self.mines);
-
-        let response = start_game(client,self.level_id).await?;
-
-        let game_data = &response.into_inner();
+        let game_data = response.into_inner();
 
         self.rows = game_data.rows;
         self.columns = game_data.columns;
+        self.mines = game_data.mines;
+
+        let test = self.click(client, 0, 0).await?;
+        println!("{:?}", test);
 
         Ok(())
     }
 
-    async fn start_game(&self, client: &mut MinesweeperClient<tonic::transport::Channel>, level_id: String) -> Result<tonic::Response<StartLevelResponse>, Box<dyn std::error::Error>> {
-        let request = tonic::Request::new(StartLevelRequest{
+    async fn start_game(
+        &self,
+        client: &mut MinesweeperClient<tonic::transport::Channel>,
+        level_id: &String,
+    ) -> Result<tonic::Response<StartLevelResponse>, Box<dyn std::error::Error>> {
+        let request = tonic::Request::new(StartLevelRequest {
             level_id: level_id.into(),
         });
-    
+
         let response: tonic::Response<StartLevelResponse> = client.start_level(request).await?;
         Ok(response)
     }
 
-    async fn click(client: &mut MinesweeperClient<tonic::transport::Channel>, level_id: String, row: i32, column: i32) -> Result<tonic::Response<ClickResponse>, Box<dyn std::error::Error>> {
-        let position = minesweeper::Position{column, row};
-    
-        let request = tonic::Request::new(ClickRequest{
-            level_id,
-            tile: Some(position)
+    async fn click(
+        &self,
+        client: &mut MinesweeperClient<tonic::transport::Channel>,
+        row: i32,
+        column: i32,
+    ) -> Result<tonic::Response<ClickResponse>, Box<dyn std::error::Error>> {
+        // Make this function modify the game representation, not return response object
+        let position = minesweeper::Position { column, row };
+
+        let request = tonic::Request::new(ClickRequest {
+            level_id: self.level_id.clone(),
+            tile: Some(position),
         });
-    
+
         let response: tonic::Response<ClickResponse> = client.click(request).await?;
-        
+
         Ok(response)
     }
-    
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let mut client = MinesweeperClient::connect("http://minesweeper:1989").await?;
 
-    let response = new_id(&mut client).await?;
-    let id = response.into_inner().level_id;
+    let mut game = new_id(&mut client).await?;
 
-    let game = Game{level_id: id, rows: 0, columns: 0, mines: 0};
+    // let response = new_id(&mut client).await?;
+    // let id = response.into_inner().level_id;
 
-    game.init();
+    game.init(&mut client).await?;
 
-    // let response = start_game(&mut client, id.clone()).await?;
-
-    // println!("RESPONSE={:?}", response);
-
-    // let game_response = response.into_inner();
-
-    // let game = Game{level_id: id, rows: game_response.rows, columns: game_response.columns, mines: game_response.mines};
-
-    // game.init();
-
-    // let response = click(&mut client, game.level_id, 3, 2).await?;
-
-    // println!("RESPONSE={:?}", response);
+    println!("{}", game);
 
     Ok(())
 }
 
-async fn new_id(client: &mut MinesweeperClient<tonic::transport::Channel>) -> Result<tonic::Response<NewGameResponse>, Box<dyn std::error::Error>> {
-    let request = tonic::Request::new(NewGameRequest{});
+async fn new_id(
+    client: &mut MinesweeperClient<tonic::transport::Channel>,
+) -> Result<Game, Box<dyn std::error::Error>> {
+    let request = tonic::Request::new(NewGameRequest {});
     let response: tonic::Response<NewGameResponse> = client.new_game(request).await?;
 
-    Ok(response)
+    let game = Game {
+        level_id: response.into_inner().level_id,
+        rows: 0,
+        columns: 0,
+        mines: 0,
+    };
+
+    Ok(game)
 }
 
-async fn start_game(client: &mut MinesweeperClient<tonic::transport::Channel>, level_id: String) -> Result<tonic::Response<StartLevelResponse>, Box<dyn std::error::Error>> {
-    let request = tonic::Request::new(StartLevelRequest{
-        level_id: level_id.into(),
-    });
-
-    let response: tonic::Response<StartLevelResponse> = client.start_level(request).await?;
-    Ok(response)
-}
-
-async fn click(client: &mut MinesweeperClient<tonic::transport::Channel>, level_id: String, row: i32, column: i32) -> Result<tonic::Response<ClickResponse>, Box<dyn std::error::Error>> {
-    let position = minesweeper::Position{column, row};
-
-    let request = tonic::Request::new(ClickRequest{
-        level_id,
-        tile: Some(position)
-    });
-
-    let response: tonic::Response<ClickResponse> = client.click(request).await?;
-    
-    Ok(response)
+impl std::fmt::Display for Game {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Rows: {}\nColumns: {}\nMines: {}",
+            self.rows, self.columns, self.mines
+        )
+    }
 }
