f72bd2a79f4d3a15c7136cd3dc01be33cfca2a85
Eric <eirtob98@hotmail.com>
2020-12-26 01:46:37 +0100

    Program stores game state in memory, and can pretty print the board

--- src/client.rs
+++ src/client.rs
@@ -13,8 +13,15 @@ pub mod minesweeper {
 struct BoardPiece {
     x: i8,
     y: i8,
-    value: i8,
+    value: i32,
 }
+
+impl std::fmt::Debug for BoardPiece {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "\nX: {} Y: {} Value: {}", self.x, self.y, self.value)
+    }
+}
+
 impl BoardPiece {
     fn new() -> BoardPiece {
         BoardPiece {
@@ -32,6 +39,49 @@ struct Game {
     mines: i32,
     board: Array2D<BoardPiece>,
 }
+
+impl std::fmt::Display for Game {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut board_as_string: String = String::from("");
+        for x in 0..self.board.row_len() {
+            for y in 0..self.board.column_len() {
+                let element: &BoardPiece = &self.board[(x, y)];
+
+                if element.y == 0 {
+                    board_as_string.push('|');
+                }
+
+                if element.value == -1 {
+                    board_as_string.push('X')
+                } else {
+                    board_as_string.push_str(&element.value.to_string()[..])
+                }
+
+                board_as_string.push('|');
+
+                if element.y == self.board.row_len() as i8 - 1 {
+                    board_as_string.push_str("\n");
+                }
+            }
+        }
+        write!(
+            f,
+            "Rows: {}, Columns: {}, Mines: {}, Board:\n{}",
+            self.rows, self.columns, self.mines, board_as_string,
+        )
+    }
+}
+
+impl std::fmt::Debug for Game {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Rows: {}\nColumns: {}\nMines: {}\nBoard:\n{:?}",
+            self.rows, self.columns, self.mines, self.board
+        )
+    }
+}
+
 impl Game {
     async fn init(
         &mut self,
@@ -39,21 +89,32 @@ impl Game {
     ) -> Result<(), Box<dyn std::error::Error>> {
         let response = self.start_game(client, &self.level_id).await?;
 
+        println!("Got response..\nSending id and getting game board");
         let game_data = response.into_inner();
 
-        println!("{:?}", self.board);
-
+        println!("Got response..");
+        self.rows = game_data.rows;
         self.columns = game_data.columns;
         self.mines = game_data.mines;
         self.board = self.build_board();
-
-        println!("{:?}", self.board);
 
         Ok(())
     }
 
     fn build_board(&mut self) -> Array2D<BoardPiece> {
-        let board = Array2D::filled_with(BoardPiece::new(), 2, 2);
+        println!("Building board..");
+        let mut board =
+            Array2D::filled_with(BoardPiece::new(), self.rows as usize, self.columns as usize);
+
+        for x in 0..board.row_len() {
+            for y in 0..board.column_len() {
+                board[(x, y)] = BoardPiece {
+                    x: x as i8,
+                    y: y as i8,
+                    value: -1,
+                }
+            }
+        }
         board
     }
 
@@ -71,12 +132,11 @@ impl Game {
     }
 
     async fn click(
-        &self,
+        &mut self,
         client: &mut MinesweeperClient<tonic::transport::Channel>,
         row: i32,
         column: i32,
-    ) -> Result<tonic::Response<ClickResponse>, Box<dyn std::error::Error>> {
-        // Make this function modify the game representation, not return response object
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let position = minesweeper::Position { column, row };
 
         let request = tonic::Request::new(ClickRequest {
@@ -86,20 +146,24 @@ impl Game {
 
         let response: tonic::Response<ClickResponse> = client.click(request).await?;
 
-        Ok(response)
+        let r = response.into_inner();
+
+        self.board[(row as usize, column as usize)].value = r.value;
+
+        Ok(())
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = MinesweeperClient::connect("http://minesweeper:1989").await?;
-
     let mut game = new_id(&mut client).await?;
 
-    // let response = new_id(&mut client).await?;
-    // let id = response.into_inner().level_id;
-
     game.init(&mut client).await?;
+
+    println!("{}", game);
+
+    game.click(&mut client, 0, 0).await?;
 
     println!("{}", game);
 
@@ -109,6 +173,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 async fn new_id(
     client: &mut MinesweeperClient<tonic::transport::Channel>,
 ) -> Result<Game, Box<dyn std::error::Error>> {
+    println!("Getting id..");
     let request = tonic::Request::new(NewGameRequest {});
     let response: tonic::Response<NewGameResponse> = client.new_game(request).await?;
 
@@ -121,20 +186,4 @@ async fn new_id(
     };
 
     Ok(game)
-}
-
-impl std::fmt::Display for Game {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "Rows: {}\nColumns: {}\nMines: {}",
-            self.rows, self.columns, self.mines
-        )
-    }
-}
-
-impl std::fmt::Debug for BoardPiece {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        writeln!(f, "X: {} Y: {} Value: {}", self.x, self.y, self.value)
-    }
 }
